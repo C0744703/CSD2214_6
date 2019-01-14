ed67bd3f729a8d8300fa5c854c81b91c986ead45
C0744703 <46692945+C0744703@users.noreply.github.com>
2019-01-14 17:11:17 -0500

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+let countDown;
+let timeInterval;
+const clock = document.querySelector('input[name="endDate"]');
+const clock = document.querySelector('#clock');
+const daysSpan = clock.querySelector('.days');
+const hoursSpan = clock.querySelector('.hours');   
+const minutesSpan = clock.querySelector('.minutes');
+const secondsSpan = clock.querySelector('.seconds');
